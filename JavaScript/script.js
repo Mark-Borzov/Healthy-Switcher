2c0fb075eee73043715d98d64ed83cb3b9b98901
Mark Borzov <stevelincoln@bk.ru>
2023-04-03 17:34:22 +0300

    The "about" And "work" Sections Are Completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,3 +16,51 @@ $(document).ready(function() {
         $('body').toggleClass('lock'); 
     });
 });
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault()
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+var swiper = new Swiper(".mySwiper",{
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 2,
+    loop: true,
+    autoplay: { 
+    delay: 1500, 
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+    },
+    speed: 1000,
+    scrollbar:{
+    el: '.swiper-scrollbar', 
+    draggable: true,
+    },
+    breakpoints:{ 
+        0:{
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        1150:{
+            slidesPerView: 2,
+            spaceBetween: 50,
+        }
+    }
+});
+
+let workItems = document.querySelectorAll(`#workItem`);
+for(let workItem of workItems){
+    workItem.addEventListener(`click`, function(){
+        this.classList.toggle(`active`);
+    });
+}
